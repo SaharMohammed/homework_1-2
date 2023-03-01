9fb602f9fe2ab75c31c1ee90addd5422d350b473
SaharMohammed <124106790+SaharMohammed@users.noreply.github.com>
2023-02-28 18:20:05 -0800

    Update push_homework

--- push_homework.sh
+++ push_homework.sh
@@ -8,18 +8,19 @@ git status
 
 # add the homework file to the repository
 git add Homework_1.txt
+git add push_homework.sh
+git add README.md
 
 # create the first commit
-git commit -m "add homework word document"
+git commit -m "adding all the files"
 
-#show commits
-git show
+git status
 
 # rename the default branch to 'main'
 git branch main
 
 # add a remote repository
-git remote add origin https://github.com/SaharMohammed/GIT_homework.git
+git remote add origin https://github.com/SaharMohammed/homework_1-2.git
 
 # push the repository to the remote
 git push origin main
